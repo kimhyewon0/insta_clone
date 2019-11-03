f71a8df554b9539843d8b2b3fc0e5c2a8d452aac
hailey <coope0357@gmail.com>
2019-11-03 18:11:41 +0900

    11.3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const heart = document.querySelector('.heart_btn');
 const header = document.querySelector('#header');
 const sidebox = document.querySelector('.side_box');
-
+const variableWidth = document.querySelectorAll('.contents_box .contents');
 
 
 heart.addEventListener('click',function () {
@@ -16,6 +16,22 @@ function resizeFunc(){
         let calcWidth = (window.innerWidth*0.5) + 175;
 
         sidebox.style.left = calcWidth + 'px';
+    }
+
+    //화면이 800px 이하가 되었을 때 실행
+    //해상도의 크기보다 커지가되면 스와이프를 할 때 좌우가 흔들리는 문제점이 생길 수 있다
+    //해결하기 위해 작성 됨. -> '좌우값을 화면크기에 맞춰서 유지시키기'
+    if(matchMedia('screen and (max-width : 800px)').matches){
+        //모든 contents를 다 작동시키기위해서 반복문 설정함
+        for(let i=0; i < variableWidth.length; i++){
+            //20은 편차!
+            variableWidth[i].style.width = window.innerWidth -20 + 'px';
+        }
+    }
+    else{ //800보다 작아졌다가 높아졌을 때 삭제하기
+        for(let i=0; i < variableWidth.length; i++){
+            variableWidth[i].removeAttribute('style')
+        }
     }
 }
 
@@ -32,8 +48,9 @@ function scrollFucn(){
         header.classList.remove('on');
         sidebox.classList.remove('on');
         // 고정된 left값을 삭제해주기 위함 -> 속성자체를 지우는게 편하다.
-        sidebox.removeAttribute('style')
+        sidebox.removeAttribute('style');
     }
 }
 // 바로 호출하지 않고 새로운 함수를 만듦
+window.addEventListener('resize',resizeFunc);
 window.addEventListener('scroll',scrollFucn);
