a290ebf7fe052279b3cc144402f8afa7e34db0c4
hailey <coope0357@gmail.com>
2019-11-15 01:25:34 +0900

    11.14

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
-const heart = document.querySelector('.heart_btn');
 const header = document.querySelector('#header');
 const sidebox = document.querySelector('.side_box');
 const variableWidth = document.querySelectorAll('.contents_box .contents');
+const delegation = document.querySelector('.contents_box');
 
 
-heart.addEventListener('click',function () {
-    heart.classList.toggle('on')
-});
+
+// event delegation 대상의 부모에게 떠넘김
+// 예를 들어 게시글이 100개 있는데 그 게시글의 모든 하트를 눌렀을때 100개의 이벤트가 필요함
+// 불필요한 이벤트 추가를 하지 않게 하기위해서 사용함
+function delegationFunc(e){
+    let elem = e.target;
+    console.log(elem);
+
+    while(!elem.getAttribute('data-name')){
+        //클릭시 데이터 네임이 없을 경우 실행
+
+        //데이터노드가 없을 경우에는 부모노드의 data-name을 찾음
+        elem = elem.parentNode;
+
+        if(elem.nodeName === 'BODY'){
+            // 부모노드가 body일 경우 null을 주고 함수종료
+            elem = null;
+            return;
+        }
+
+    }
+
+    if(elem.matches('[data-name="heartbeat"]')){
+        //클릭한 엘리먼트의 대상의 데이터 네임속성이 heartbeat라는 속성을 가지고 있을 때 이 문장이 실행됨
+        console.log('하트');
+    }else if(elem.matches('[data-name="bookmark"]')){
+        console.log('북마크');
+    }else if(elem.matches('[data-name="share"]')){
+        console.log('공유');
+    }else if(elem.matches('[data-name="moer"]')){
+        console.log('더보기');
+    }
+
+    elem.classList.toggle('on');
+
+}
 
 function resizeFunc(){
     if(pageYOffset >= 10){
@@ -30,27 +63,45 @@ function resizeFunc(){
     }
     else{ //800보다 작아졌다가 높아졌을 때 삭제하기
         for(let i=0; i < variableWidth.length; i++){
-            variableWidth[i].removeAttribute('style')
+            if(window.innerWidth>600){
+                variableWidth[i].removeAttribute('style');
+            }
         }
     }
 }
 
 function scrollFucn(){
     //스크롤 할때마다 위치를 출력해줌
-    console.log(pageYOffset);
+    // console.log(pageYOffset);
     if (pageYOffset >= 10){
         header.classList.add('on');
-        sidebox.classList.add('on');
+        if (sidebox){
+            sidebox.classList.add('on')
+        }
 
         resizeFunc();
     }
     else{
         header.classList.remove('on');
-        sidebox.classList.remove('on');
+        if (sidebox){
+            sidebox.classList.remove('on');
+
+        }
         // 고정된 left값을 삭제해주기 위함 -> 속성자체를 지우는게 편하다.
         sidebox.removeAttribute('style');
     }
 }
+
+setTimeout(function () {
+    scrollTo(0,0)
+},100)
 // 바로 호출하지 않고 새로운 함수를 만듦
+
+if(delegation){
+    window.addEventListener('click',delegationFunc);
+
+}
 window.addEventListener('resize',resizeFunc);
 window.addEventListener('scroll',scrollFucn);
+
+
